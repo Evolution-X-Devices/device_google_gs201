f5da646328a2a71e4eec28044a14e17303ff4179
AnierinB <anierin@evolution-x.org>
2022-11-04 20:46:06 +0000

    gs201: Initialize Evolution X 8.x
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- evolution_common.mk
+++ evolution_common.mk
@@ -5,7 +5,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += device/google/gs201/overlay-evolution
 
 # AiAi Config
 PRODUCT_COPY_FILES += \
@@ -16,16 +16,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
-# GMS
-ifeq ($(WITH_GMS),true)
-GMS_MAKEFILE=gms_64bit_only.mk
-endif
-
 # Google Assistant
 PRODUCT_PRODUCT_PROPERTIES += ro.opa.eligible_device=true
-
-# Lineage Health
-include hardware/google/pixel/lineage_health/device.mk
 
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
